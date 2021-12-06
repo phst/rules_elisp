5ed144da393fccb5e0fdaa5b8418d427967cb6d2
Philipp Stephani <phst@google.com>
2021-12-07 00:00:05 +0100

    Add a test using the ‘ert-resource-file’ macro

--- examples/lib-1-test.el
+++ examples/lib-1-test.el
@@ -1,6 +1,6 @@
 ;;; lib-1-test.el --- unit test for lib-1 -*- lexical-binding: t; -*-
 
-;; Copyright 2020 Google LLC
+;; Copyright 2020, 2021 Google LLC
 ;;
 ;; Licensed under the Apache License, Version 2.0 (the "License");
 ;; you may not use this file except in compliance with the License.
@@ -23,6 +23,7 @@
 (require 'examples/lib-1)
 
 (require 'ert)
+(require 'ert-x)
 
 (ert-deftest lib-1-test ()
   (should (equal (lib-1-func) "hi from lib-1")))
@@ -31,5 +32,13 @@
   ;; This test is broken, but Bazel will skip it due to the “skip_tests”
   ;; attribute in the build rule.
   (should (= 0 1)))
+
+(ert-deftest lib-1-resource ()
+  (skip-unless (fboundp 'ert-resource-file))
+  (with-temp-buffer
+    (insert-file-contents (ert-resource-file "test.txt"))
+    (should (equal (buffer-string) "hi from ERT resource\n"))))
+
+(declare-function ert-resource-file "ert-x" (file))
 
 ;;; lib-1-test.el ends here
