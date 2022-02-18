663e0dd2f1c241965da383d6d9899ee12f80cbb1
Philipp Stephani <phst@google.com>
2022-02-18 19:54:57 +0100

    We never use tabs for indentation.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,8 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((fill-column . 80)))
+((nil . ((indent-tabs-mode . nil)
+         (fill-column . 80)))
  (c++-mode . ((page-delimiter . "^///")
               (mode . subword)))
  (c-mode . ((page-delimiter . "^///")
