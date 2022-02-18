f74de979a8cfdb395b32c07e4348b279e9cf730a
Philipp Stephani <phst@google.com>
2022-02-18 16:24:01 +0100

    Enable subword-mode in C/C++ files.
    This matches the CamelCase style.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((fill-column . 80))))
+((nil . ((fill-column . 80)))
+ (c++-mode . ((mode . subword)))
+ (c-mode . ((mode . subword))))
