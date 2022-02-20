257d106bedd4f3a01e139676b2fa70833ae74f0c
Philipp Stephani <phst@google.com>
2022-02-20 22:03:45 +0100

    add license headers to documentation files

--- docs/org.py
+++ docs/org.py
@@ -37,6 +37,8 @@ def _main() -> None:
     # Force Unix-style line endings for consistent results.  See
     # https://github.com/bazelbuild/stardoc/issues/110.
     with opts.output.open(mode='xt', encoding='utf-8', newline='\n') as file:
+        file.write(_LICENSE)
+        file.write('\n')
         _markdown(file, module.module_docstring)
         for rule in module.rule_info:
             file.write(f'* ~{rule.rule_name}~ rule\n\n')
@@ -70,6 +72,21 @@ def _main() -> None:
                 file.write(f'This aspect propagates along the following '
                            f'attributes: {attrs}\n')
             _attributes(file, aspect.attribute)
+
+_LICENSE = """# Copyright 2020, 2021, 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
 
 def _attributes(file: io.TextIOBase,
                 attributes: Sequence[stardoc_output_pb2.AttributeInfo]) -> None:
