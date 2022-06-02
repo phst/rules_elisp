915d5faf5e120e5737c65b45d38d488d9035af6c
Philipp Stephani <phst@google.com>
2022-06-02 14:22:55 +0200

    Refer to Emacs bugs by default

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,8 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((indent-tabs-mode . nil)
+((nil . ((bug-reference-url-format . "https://bugs.gnu.org/%s")
+         (indent-tabs-mode . nil)
          (fill-column . 80)))
  (c++-mode . ((page-delimiter . "^///")
               (mode . subword)))
