9a8c6e74a0453b284c490ac37f339b1dc49f3d90
Philipp Stephani <phst@google.com>
2022-06-02 14:36:49 +0200

    Use canonical format for .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -12,4 +12,4 @@
  ("elisp/runfiles/" . ((emacs-lisp-mode . ((elisp-flymake-byte-compile-load-path "../../")))))
  ("examples/" . ((emacs-lisp-mode . ((elisp-flymake-byte-compile-load-path "../" "./" "ext/" "../bazel-bin/" "../bazel-bin/external/com_google_protobuf/")))))
  ("tests/" . ((emacs-lisp-mode . ((elisp-flymake-byte-compile-load-path "../")))))
- ("tests/pkg/" . ((emacs-lisp-mode . ((elisp-flymake-byte-compile-load-path . ("../../")))))))
+ ("tests/pkg/" . ((emacs-lisp-mode . ((elisp-flymake-byte-compile-load-path "../../"))))))
