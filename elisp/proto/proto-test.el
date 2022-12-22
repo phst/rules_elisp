cff70b044c30d65abcaeb974153d6735e0c21282
Philipp Stephani <phst@google.com>
2022-12-22 13:29:36 +0100

    Revert "Add support for outputting enumerator values in JSON"
    This isn't supported in the release branch yet.
    
    This reverts commit a7454084f8f2d591bc598ddef9fbc387e5e803ee.

--- elisp/proto/proto-test.el
+++ elisp/proto/proto-test.el
@@ -881,16 +881,6 @@
                            (string-join (make-vector #x2000 "1") ",")
                            "]}")))))
 
-(ert-deftest elisp/proto/serialize-json/enum-numbers ()
-  (let ((message (elisp/proto/Test-new :enumeration elisp/proto/ENUM_BAR)))
-    (pcase-dolist (`(,enum-numbers ,expected)
-                   '((nil "{\"enumeration\":\"ENUM_BAR\"}")
-                     (t "{\"enumeration\":1}")))
-      (ert-info ((prin1-to-string enum-numbers) :prefix "enum-numbers: ")
-        (should (equal (elisp/proto/serialize-json message
-                                                   :enum-numbers enum-numbers)
-                       expected))))))
-
 (ert-deftest elisp/proto/any ()
   (let* ((message (elisp/proto/make-duration 123))
          (any (elisp/proto/pack-any message)))
