81a7b6aae571dab09d54a7465b7767409b7a3c82
Philipp Stephani <phst@google.com>
2023-04-12 16:54:16 +0200

    Test for minimum required C++ version

--- elisp/test.h
+++ elisp/test.h
@@ -1,4 +1,4 @@
-// Copyright 2020, 2021, 2022 Google LLC
+// Copyright 2020, 2021, 2022, 2023 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,6 +14,10 @@
 
 #ifndef PHST_RULES_ELISP_ELISP_TEST_H
 #define PHST_RULES_ELISP_ELISP_TEST_H
+
+#if !defined __cplusplus || __cplusplus < 201103L
+#error this file requires at least C++11
+#endif
 
 #include <vector>
 
