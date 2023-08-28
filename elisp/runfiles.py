8a73236164ac536f3f235e89f9770e215e0b31ee
Philipp Stephani <phst@google.com>
2023-08-28 22:03:18 +0200

    Use runfiles library from rules_python.
    The one built into Bazel doesn’t support repository mappings (and therefore
    doesn’t support modules).

--- elisp/runfiles.py
+++ elisp/runfiles.py
@@ -1,4 +1,4 @@
-# Copyright 2021, 2022 Google LLC
+# Copyright 2021, 2022, 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ import os
 import pathlib
 from typing import Optional
 
-from tools.python.runfiles import runfiles
+from python.runfiles import runfiles
 
 class Runfiles:
     """Represents a set of Bazel runfiles."""
