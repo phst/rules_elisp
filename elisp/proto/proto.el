625974d7e9f7ca759f74b2691718fefb2abfa6af
Allen Li <ayatane@google.com>
2023-12-19 18:15:26 -0800

    Add package version metadata
    Needed for a future commit to add package.el support.

--- elisp/proto/proto.el
+++ elisp/proto/proto.el
@@ -14,6 +14,8 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
+;; Version: 0.1.0
+
 ;;; Commentary:
 
 ;; Provides basic support for protocol buffers.
--- tests/test-lib.el
+++ tests/test-lib.el
@@ -14,6 +14,8 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
+;; Version: 0.1.0
+
 ;;; Commentary:
 
 ;; Example functions to test coverage reporting.  This has to be in a separate
