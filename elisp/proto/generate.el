0a275d74a9af5f949be415f08a7d769cdc9f0a7d
Allen Li <ayatane@google.com>
2023-12-19 18:15:26 -0800

    Add package version metadata
    Needed for a future commit to add package.el support.

--- elisp/proto/generate.el
+++ elisp/proto/generate.el
@@ -14,6 +14,8 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
+;; Version: 0.1.0
+
 ;;; Commentary:
 
 ;; Generates Emacs Lisp bindings for a protocol buffer definition.
