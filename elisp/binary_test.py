a0739e09c452fe7bd9ec8d19a0c3447a34d21ea3
Philipp Stephani <p.stephani2@gmail.com>
2024-01-15 13:56:31 +0100

    Convert //elisp:binary_test to Python

---
+++ elisp/binary_test.py
@@ -0,0 +1,51 @@
+# Copyright 2020, 2021, 2022, 2023, 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests for the elisp_binary rule."""
+
+import pathlib
+import platform
+import subprocess
+
+from absl import flags
+from absl.flags import FLAGS
+from absl.testing import absltest
+
+from elisp import runfiles
+
+
+flags.DEFINE_string('binary', '', 'location of the //elisp:binary_main target')
+
+
+class BinaryTest(absltest.TestCase):
+    """Unit tests for the elisp_binary rule."""
+
+    def test_run_wrapped(self) -> None:
+        """Test that running a binary with a wrapper works."""
+        run_files = runfiles.Runfiles()
+        input_file = run_files.resolve(
+            pathlib.PurePosixPath('phst_rules_elisp/elisp/binary.cc'))
+        windows = platform.system() == 'Windows'
+        args = [
+            run_files.resolve(pathlib.PurePosixPath(FLAGS.binary)),
+            '--option',
+            input_file,
+            ''' \t\n\r\f äα𝐴🐈'\\\"''',
+            r'/:C:\Temp\output.dat' if windows else '/:/tmp/output.dat',
+        ]
+        subprocess.run(args, check=True)
+
+
+if __name__ == '__main__':
+    absltest.main()
