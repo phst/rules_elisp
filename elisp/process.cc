d585f3255d6ff8d99e764bed5112013e344ecd6e
Philipp Stephani <p.stephani2@gmail.com>
2024-02-26 19:10:50 +0100

    Add missing dependency

--- elisp/process.cc
+++ elisp/process.cc
@@ -66,6 +66,7 @@
 #include "absl/base/attributes.h"
 #include "absl/base/nullability.h"
 #include "absl/cleanup/cleanup.h"  // IWYU pragma: keep
+#include "absl/hash/hash.h"        // IWYU pragma: keep
 #include "absl/log/check.h"
 #include "absl/log/log.h"  // IWYU pragma: keep
 #include "absl/status/status.h"
