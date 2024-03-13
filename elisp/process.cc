987df7a6c82de012cddd3bb036069abfb4ed0e2f
Philipp Stephani <p.stephani2@gmail.com>
2024-03-13 02:46:59 +0100

    Make runfile filenames absolute

--- elisp/process.cc
+++ elisp/process.cc
@@ -40,6 +40,7 @@
 #include <algorithm>  // IWYU pragma: keep
 #include <cerrno>     // IWYU pragma: keep
 #include <cstddef>
+#include <cstdlib>
 #include <limits>
 #include <memory>
 #include <optional>
@@ -344,6 +345,24 @@ static absl::StatusOr<NativeString> ToNative(const std::string_view string) {
   return ConvertString<NativeString>(string);
 }
 
+#ifndef _WIN32
+static absl::StatusOr<std::string> WorkingDirectory() {
+  struct Free {
+    void operator()(const absl::Nonnull<char*> p) const { std::free(p); }
+  };
+  // Assume that we always run on an OS that allocates a buffer when passed a
+  // null pointer.
+  const absl::Nullable<std::unique_ptr<char, Free>> ptr(getcwd(nullptr, 0));
+  if (ptr == nullptr) return ErrnoStatus("getcwd", nullptr, 0);
+  // See the Linux man page for getcwd(3) why this can happen.
+  if (*ptr != '/') {
+    return absl::NotFoundError(absl::StrCat("Current working directory ",
+                                            ptr.get(), " is unreachable"));
+  }
+  return ptr.get();
+}
+#endif
+
 class EnvironmentBlock final {
  private:
   struct Free;
@@ -533,7 +552,32 @@ static absl::StatusOr<NativeString> ResolveRunfile(
 #ifdef _WIN32
   absl::c_replace(resolved, '/', '\\');
 #endif
-  return ToNative(resolved);
+  absl::StatusOr<NativeString> native = ToNative(resolved);
+  if (!native.ok()) return native.status();
+#ifdef _WIN32
+  // 0x8000 is the maximum length of a filename on Windows.  See
+  // https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-getfullpathnamew#parameters.
+  constexpr DWORD size = 0x8000;
+  std::wstring buffer(size, L'\0');
+  const DWORD result =
+      ::GetFullPathNameW(Pointer(*native), size, buffer.data(), nullptr);
+  if (result == 0) {
+    return WindowsStatus("GetFullPathNameW", Escape(*native), size, "...",
+                         nullptr);
+  }
+  if (result > size) {
+    return absl::OutOfRangeError(
+        absl::StrCat("Length of absolute file name (", result,
+                     ") overflows buffer of size ", size));
+  }
+  return buffer.substr(0, result);
+#else
+  CHECK(!native->empty());
+  if (native->front() == '/') return native;
+  const absl::StatusOr<std::string> cwd = WorkingDirectory();
+  if (!cwd.ok()) return cwd.status();
+  return absl::StrCat(*cwd, "/", *native);
+#endif
 }
 
 static absl::StatusOr<Environment> RunfilesEnvironment(
