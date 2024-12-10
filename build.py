8af8a653a54ebc0c37c59023bf27f8b02ae71a8c
Philipp Stephani <p.stephani2@gmail.com>
2024-12-10 16:47:08 +0100

    Stop testing legacy WORKSPACE mode

--- build.py
+++ build.py
@@ -69,17 +69,6 @@ class Builder:
             os.getenv('BUILD_WORKSPACE_DIRECTORY')
             or pathlib.Path(__file__).parent
         ).absolute()
-        version_str = subprocess.run([str(bazel), '--version'], check=True,
-                                     stdout=subprocess.PIPE,
-                                     encoding='utf-8').stdout
-        # https://www.gnu.org/prep/standards/html_node/_002d_002dversion.html
-        match = re.match(r'.+ (\d+)\.(\d+)[^ ]*$', version_str,
-                         re.ASCII | re.MULTILINE)
-        if not match:
-            raise ValueError(f'invalid version string {version_str}')
-        version = (int(match[1]), int(match[2]))
-        # Older Bazel versions don’t support the --enable_workspace option.
-        self._enable_workspace = version >= (7, 1)
 
     def build(self, goals: Sequence[str]) -> None:
         """Builds the specified goals."""
@@ -154,21 +143,17 @@ class Builder:
 
     def _test(self, *args: str, profile: str,
               cwd: Optional[pathlib.Path] = None) -> None:
-        for bzlmod in (True, False):
-            prefix = '' if bzlmod else 'no'
-            options = [f'--{prefix}enable_bzlmod']
-            if not bzlmod and self._enable_workspace:
-                options.append('--enable_workspace')
-            if bzlmod and self._profiles:
-                self._profiles.mkdir(exist_ok=True)
-                profile_file = self._profiles / (profile + '.json.gz')
-                options += [
-                    '--generate_json_trace_profile',
-                    '--experimental_announce_profile_path',
-                    '--profile=' + str(profile_file),
-                ]
-            options.extend(args)
-            _run([self._bazel, 'test'] + options + ['--', '//...'], cwd=cwd)
+        options = [f'--enable_bzlmod']
+        if self._profiles:
+            self._profiles.mkdir(exist_ok=True)
+            profile_file = self._profiles / (profile + '.json.gz')
+            options += [
+                '--generate_json_trace_profile',
+                '--experimental_announce_profile_path',
+                '--profile=' + str(profile_file),
+            ]
+        options.extend(args)
+        _run([self._bazel, 'test'] + options + ['--', '//...'], cwd=cwd)
 
     @target
     def ext(self) -> None:
