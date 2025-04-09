425c2d1c0557fb4006ca2cc6d9293699605ecab9
Philipp Stephani <p.stephani2@gmail.com>
2025-04-09 15:40:35 +0200

    Revert "Add a test that we don’t accidentally try to override production dependencies"
    This reverts commit 726890338669f8193eab7fae314af5dc75002d39.
    
    The test doesn't pull its weight.

--- dev/version_test.go
+++ dev/version_test.go
@@ -1,4 +1,4 @@
-// Copyright 2023, 2024 Google LLC
+// Copyright 2023, 2024, 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,45 +15,8 @@
 package version_test
 
 import (
-	"strings"
-	"testing"
-
 	_ "embed"
-
-	"github.com/bazelbuild/buildtools/build"
 )
-
-func TestOverrides(t *testing.T) {
-	file, err := build.ParseModule("MODULE.bazel", moduleContent)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	deps := make(map[string]bool)
-	for _, rule := range file.Rules("bazel_dep") {
-		deps[rule.Name()] = rule.AttrLiteral("dev_dependency") == "True"
-	}
-	if len(deps) == 0 {
-		t.Error("no dependencies found")
-	}
-
-	for _, rule := range file.Rules("") {
-		kind := rule.Kind()
-		if !strings.HasSuffix(kind, "_override") {
-			continue
-		}
-		name := rule.AttrString("module_name")
-		dev, ok := deps[name]
-		if !ok {
-			t.Errorf("unknown dependency %s overridden by %s", name, kind)
-		}
-		// Overrides only work in root modules, so this dependency
-		// wouldn’t work outside of our own workspace.
-		if !dev {
-			t.Errorf("production dependency %s is overridden using %s", name, kind)
-		}
-	}
-}
 
 //go:embed MODULE.bazel
 var moduleContent []byte
