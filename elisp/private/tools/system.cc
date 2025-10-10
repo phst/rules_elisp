771e76bedd6786c9ae3c339192cf3f6fb9b7d30d
Philipp Stephani <p.stephani2@gmail.com>
2025-10-10 22:30:30 +0200

    Factor out status helpers into separate library

---
+++ elisp/private/tools/system.cc
@@ -0,0 +1,75 @@
+// Copyright 2020-2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "elisp/private/tools/system.h"
+
+#ifdef _WIN32
+#  ifndef UNICODE
+#    define UNICODE
+#  endif
+#  ifndef _UNICODE
+#    define _UNICODE
+#  endif
+#  ifndef STRICT
+#    define STRICT
+#  endif
+#  ifndef NOMINMAX
+#    define NOMINMAX
+#  endif
+#  ifndef WIN32_LEAN_AND_MEAN
+#    define WIN32_LEAN_AND_MEAN
+#  endif
+#  include <windows.h>
+#endif
+
+#include <cerrno>
+#include <optional>
+#include <string_view>
+#include <system_error>
+
+#include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
+
+#include "elisp/private/tools/numeric.h"
+
+namespace rules_elisp {
+
+absl::Status MakeErrorStatus(const std::error_code& code,
+                             const std::string_view function,
+                             const std::string_view args) {
+  if (!code) return absl::OkStatus();
+  const std::error_condition condition = code.default_error_condition();
+  const std::string message = absl::StrCat(
+      function, args.empty() ? args : absl::StrCat("(", args, ")"), ": ",
+      code.category().name(), "/", code.value(), ": ", code.message());
+  return condition.category() == std::generic_category()
+             ? absl::ErrnoToStatus(condition.value(), message)
+             : absl::UnknownError(message);
+}
+
+#ifdef _WIN32
+[[nodiscard]] std::error_code WindowsError() {
+  const DWORD code = ::GetLastError();
+  const std::optional<int> i = CastNumberOpt<int>(code);
+  return i.has_value() ? std::error_code(*i, std::system_category())
+                       : std::make_error_code(std::errc::value_too_large);
+}
+#else
+[[nodiscard]] std::error_code ErrnoError() {
+  const int code = errno;
+  return std::error_code(code, std::system_category());
+}
+#endif
+
+}
