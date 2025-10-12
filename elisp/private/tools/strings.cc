3b6c58ae5164cd2caad3366cad770b5e295de83e
Philipp Stephani <p.stephani2@gmail.com>
2025-10-13 01:41:55 +0200

    Move strings utility implementation out of header file

--- elisp/private/tools/strings.cc
+++ elisp/private/tools/strings.cc
@@ -16,7 +16,13 @@
 
 #include <optional>
 #include <string>
+#include <string_view>
+#include <type_traits>
 
+#include "absl/algorithm/container.h"
+#include "absl/log/check.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
 #include "absl/strings/ascii.h"
 #include "absl/strings/str_cat.h"
 
@@ -41,6 +47,67 @@ std::string Escape(const NativeStringView string) {
 #else
   return std::string(string);
 #endif
+}
+
+namespace {
+
+inline constexpr unsigned int kMaxASCII{0x7F};
+
+template <typename Char>
+absl::Status DoCheckASCII(const std::basic_string_view<Char> string) {
+  using Traits = typename std::basic_string_view<Char>::traits_type;
+  const auto it = absl::c_find_if(string, [](const Char ch) {
+    return Traits::lt(ch, Char{0}) || Traits::lt(Char{kMaxASCII}, ch);
+  });
+  if (it != string.end()) {
+    const auto val = static_cast<std::make_unsigned_t<Char>>(*it);
+    return absl::InvalidArgumentError(
+        absl::StrCat("non-ASCII character U+", absl::Hex(val, absl::kZeroPad4),
+                     " in string"));
+  }
+  return absl::OkStatus();
+}
+
+// Convert strings between std::string and std::wstring.  This is only useful on
+// Windows, where the native string type is std::wstring.  Only pure ASCII
+// strings are supported so that we don’t have to deal with codepages.  All
+// Windows codepages should be ASCII-compatible;
+// cf. https://docs.microsoft.com/en-us/windows/win32/intl/code-pages.
+template <typename ToString, typename FromChar>
+absl::StatusOr<ToString> ConvertString(
+    const std::basic_string_view<FromChar> string) {
+  using ToChar = typename ToString::value_type;
+  if constexpr (std::is_same_v<FromChar, ToChar>) return ToString(string);
+  static_assert(InRange<ToChar>(kMaxASCII),
+                "destination character type too small");
+  const absl::Status status = CheckASCII(string);
+  if (!status.ok()) return status;
+  ToString ret;
+  ret.reserve(string.length());
+  for (FromChar ch : string) {
+    const std::optional<ToChar> to = CastNumberOpt<ToChar>(ch);
+    CHECK(to.has_value()) << "character " << ch << " too large";
+    ret.push_back(*to);
+  }
+  return ret;
+}
+
+}  // namespace
+
+absl::Status CheckASCII(const std::string_view string) {
+  return DoCheckASCII(string);
+}
+
+absl::Status CheckASCII(const std::wstring_view string) {
+  return DoCheckASCII(string);
+}
+
+absl::StatusOr<std::string> ToNarrow(const NativeStringView string) {
+  return ConvertString<std::string>(string);
+}
+
+absl::StatusOr<NativeString> ToNative(const std::string_view string) {
+  return ConvertString<NativeString>(string);
 }
 
 }  // namespace rules_elisp
