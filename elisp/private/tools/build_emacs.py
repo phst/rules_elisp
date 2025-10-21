ea6902ab7d1c166577c8394f366d2f1458f22a4a
Philipp Stephani <p.stephani2@gmail.com>
2025-10-21 02:52:36 +0200

    Revert "Support Emacs source archives in emacs_binary."
    This reverts commit e0343eda73f9a0b4e8a1dee0996b3dddb8d84b82.

--- elisp/private/tools/build_emacs.py
+++ elisp/private/tools/build_emacs.py
@@ -20,7 +20,6 @@ use it outside the rules or depend on its behavior.
 
 import argparse
 from collections.abc import Set
-import datetime
 import glob
 import json
 import os
@@ -38,10 +37,7 @@ def main() -> None:
     """Configures and builds Emacs."""
     parser = argparse.ArgumentParser(allow_abbrev=False)
     parser.add_argument('--release', action='store_true')
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--archive', type=pathlib.Path)
-    group.add_argument('--readme', type=pathlib.Path)
-    parser.add_argument('--strip-prefix', type=pathlib.PurePosixPath)
+    parser.add_argument('--readme', type=pathlib.Path, required=True)
     parser.add_argument('--install', type=pathlib.Path, required=True)
     parser.add_argument('--bash', type=pathlib.Path)
     parser.add_argument('--cc', type=pathlib.Path)
@@ -50,9 +46,7 @@ def main() -> None:
     parser.add_argument('--module-header', type=pathlib.Path)
     parser.add_argument('--builtin-features', type=pathlib.Path)
     args = parser.parse_args()
-    source = (args.readme.resolve().parent
-              if args.readme
-              else (args.archive, args.strip_prefix))
+    source = args.readme.resolve().parent
     install = args.install.resolve()
 
     if args.release:
@@ -77,19 +71,13 @@ def main() -> None:
         shutil.copy(install / 'include' / 'emacs-module.h', args.module_header)
 
 
-def _build(*, source: pathlib.Path | tuple[pathlib.Path, pathlib.PurePosixPath],
-           install: pathlib.Path, bash: pathlib.Path,
+def _build(*, source: pathlib.Path, install: pathlib.Path, bash: pathlib.Path,
            cc: pathlib.Path, cflags: str, ldflags: str,
            builtin_features: bool) -> Optional[Set[str]]:
     windows = platform.system() == 'Windows'
     temp = pathlib.Path(tempfile.mkdtemp(prefix='emacs-build-'))
     build = temp / 'build'
-
-    if isinstance(source, tuple):
-        archive, prefix = source
-        _unpack_archive(archive, build, prefix=prefix)
-    else:
-        shutil.copytree(source, build)
+    shutil.copytree(source, build)
 
     def run(*command: str) -> None:
         env = None
@@ -159,8 +147,7 @@ def _build(*, source: pathlib.Path | tuple[pathlib.Path, pathlib.PurePosixPath],
     return features
 
 
-def _unpack(*, source: tuple[pathlib.Path, pathlib.PurePosixPath],
-            install: pathlib.Path,
+def _unpack(*, source: pathlib.Path, install: pathlib.Path,
             builtin_features: bool) -> Optional[Set[str]]:
     try:
         # Bazel sometimes creates the output directory; remove it so that
@@ -169,11 +156,7 @@ def _unpack(*, source: tuple[pathlib.Path, pathlib.PurePosixPath],
     except FileNotFoundError:
         pass
 
-    if isinstance(source, tuple):
-        archive, prefix = source
-        _unpack_archive(archive, install, prefix=prefix)
-    else:
-        shutil.copytree(source, install)
+    shutil.copytree(source, install)
 
     lisp = _glob_unique(install / 'share' / 'emacs' / '[0-9]*' / 'lisp')
     features = _builtin_features(lisp) if builtin_features else None
@@ -207,40 +190,6 @@ def _rename(src: pathlib.Path, dest: pathlib.Path) -> pathlib.Path:
     ret = src.resolve(strict=True).rename(dest)
     src.unlink(missing_ok=True)
     return ret
-
-
-def _unpack_archive(archive: pathlib.Path, dest: pathlib.Path, *,
-                    prefix: Optional[pathlib.PurePosixPath] = None) -> None:
-    if dest.exists(follow_symlinks=False):
-        raise FileExistsError(f'destination directory {dest} already exists')
-    prefix = prefix or pathlib.PurePosixPath()
-    if prefix.is_absolute():
-        raise ValueError(f'absolute prefix {prefix}')
-    temp = pathlib.Path(tempfile.mkdtemp(prefix='emacs-unpack-'))
-    shutil.unpack_archive(archive, temp)
-    shutil.move(temp / prefix, dest, copy_function=shutil.copy)
-    shutil.rmtree(temp, ignore_errors=True)
-    _touch(dest, datetime.datetime(2020, 1, 1, tzinfo=datetime.timezone.utc))
-
-
-def _touch(root: pathlib.Path, time: datetime.datetime) -> None:
-    stamp = time.timestamp()
-    for parent, _dirs, files in root.walk(on_error=_raise):
-        path = pathlib.Path(parent)
-        for file in files:
-            try:
-                os.utime(path / file, (stamp, stamp))
-            except FileNotFoundError:
-                # This can happen on Windows if the filename is too long, see
-                # https://learn.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation
-                # and
-                # https://docs.python.org/3/using/windows.html#removing-the-max-path-limitation.
-                # Nothing we can do about it.
-                pass
-
-
-def _raise(ex: OSError) -> None:
-    raise ex
 
 
 if __name__ == '__main__':
