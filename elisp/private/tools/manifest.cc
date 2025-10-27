d8548454ed7786a895645991d8ff8b570b5ad659
Philipp Stephani <p.stephani2@gmail.com>
2025-10-27 06:08:57 +0100

    Fix argument

--- elisp/private/tools/manifest.cc
+++ elisp/private/tools/manifest.cc
@@ -40,7 +40,7 @@ static absl::StatusOr<std::vector<FileName>> ConvertNames(
     if (!name.ok()) return name.status();
     if (IsAbsolute(name->string())) {
       return absl::InvalidArgumentError(
-          absl::StrFormat("Filename %v is absolute", file));
+          absl::StrFormat("Filename %v is absolute", name));
     }
    result.push_back(std::move(*name));
   }
