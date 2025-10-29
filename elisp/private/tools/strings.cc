1084ddd92a7858b7e3545d01cdc06137842405a5
Philipp Stephani <p.stephani2@gmail.com>
2025-10-30 00:03:13 +0100

    Add missing dependency on StrFormat

--- elisp/private/tools/strings.cc
+++ elisp/private/tools/strings.cc
@@ -27,6 +27,7 @@
 #include "absl/status/status.h"
 #include "absl/strings/ascii.h"
 #include "absl/strings/str_cat.h"
+#include "absl/strings/str_format.h"
 
 #include "elisp/private/tools/numeric.h"
 
