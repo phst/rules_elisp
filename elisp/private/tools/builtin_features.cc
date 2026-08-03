47e52392e2fb023bd41660511251429326cb7c67
Philipp Stephani <p.stephani2@gmail.com>
2026-08-03 17:15:02 +0200

    Add missing #include

--- elisp/private/tools/builtin_features.cc
+++ elisp/private/tools/builtin_features.cc
@@ -1,4 +1,4 @@
-// Copyright 2020-2025 Google LLC
+// Copyright 2020-2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@
 #include <ios>
 #include <locale>
 #include <string>
+#include <string_view>
 #include <vector>
 
 #include "absl/algorithm/container.h"
