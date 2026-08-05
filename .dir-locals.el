cc846179266184da3ff8cd697e928a06d342d2e3
Philipp Stephani <p.stephani2@gmail.com>
2026-08-05 13:45:58 +0200

    Don’t disable indent-tabs-mode globally
    Tabs are important in Makefiles.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
-;; Copyright 2021-2023, 2025 Google LLC
+;; Copyright 2021-2023, 2025, 2026 Google LLC
 ;;
 ;; Licensed under the Apache License, Version 2.0 (the "License");
 ;; you may not use this file except in compliance with the License.
@@ -15,8 +15,7 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((indent-tabs-mode . nil)
-         (fill-column . 80)))
+((nil . ((fill-column . 80)))
  (c++-mode . ((page-delimiter . "^///")
               (mode . subword)))
  (c-mode . ((page-delimiter . "^///")
