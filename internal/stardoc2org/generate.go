2de233b44ba0690630fcfa4ccd988b5d46f4eeea
Philipp Stephani <p.stephani2@gmail.com>
2026-09-07 16:54:49 +0200

    Shorten code using helper function

--- internal/stardoc2org/generate.go
+++ internal/stardoc2org/generate.go
@@ -263,25 +263,25 @@ type markdownRenderer struct {
 func newRenderer() *markdownRenderer {
 	orgRenderer := newOrgRenderer()
 	helper := new(renderer.HelperBuilder[io.Writer, rendererConfig]).Options(
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindDocument, renderer.NodeRendererFunc(orgRenderer.document)),
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindText, renderer.NodeRendererFunc(orgRenderer.text)),
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindParagraph, renderer.NodeRendererFunc(orgRenderer.paragraph)),
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindList, renderer.NodeRendererFunc(orgRenderer.list)),
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindListItem, renderer.NodeRendererFunc(orgRenderer.item)),
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindEmphasis, renderer.NodeRendererFunc(orgRenderer.emph)),
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindCodeSpan, renderer.NodeRendererFunc(orgRenderer.code)),
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindCodeBlock, renderer.NodeRendererFunc(orgRenderer.codeBlock)),
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindLink, renderer.NodeRendererFunc(orgRenderer.link)),
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindRawHTML, renderer.NodeRendererFunc(orgRenderer.htmlInline)),
+		withNodeRenderer(ast.KindDocument, orgRenderer.document),
+		withNodeRenderer(ast.KindText, orgRenderer.text),
+		withNodeRenderer(ast.KindParagraph, orgRenderer.paragraph),
+		withNodeRenderer(ast.KindList, orgRenderer.list),
+		withNodeRenderer(ast.KindListItem, orgRenderer.item),
+		withNodeRenderer(ast.KindEmphasis, orgRenderer.emph),
+		withNodeRenderer(ast.KindCodeSpan, orgRenderer.code),
+		withNodeRenderer(ast.KindCodeBlock, orgRenderer.codeBlock),
+		withNodeRenderer(ast.KindLink, orgRenderer.link),
+		withNodeRenderer(ast.KindRawHTML, orgRenderer.htmlInline),
 
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindAutoLink, renderer.NodeRendererFunc(orgRenderer.unknown)),
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindBlockquote, renderer.NodeRendererFunc(orgRenderer.unknown)),
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindHTMLBlock, renderer.NodeRendererFunc(orgRenderer.unknown)),
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindHeading, renderer.NodeRendererFunc(orgRenderer.unknown)),
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindImage, renderer.NodeRendererFunc(orgRenderer.unknown)),
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindLinkReferenceDefinition, renderer.NodeRendererFunc(orgRenderer.unknown)),
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindStrong, renderer.NodeRendererFunc(orgRenderer.unknown)),
-		renderer.WithNodeRenderer[io.Writer, rendererConfig](ast.KindThematicBreak, renderer.NodeRendererFunc(orgRenderer.unknown)),
+		withNodeRenderer(ast.KindAutoLink, orgRenderer.unknown),
+		withNodeRenderer(ast.KindBlockquote, orgRenderer.unknown),
+		withNodeRenderer(ast.KindHTMLBlock, orgRenderer.unknown),
+		withNodeRenderer(ast.KindHeading, orgRenderer.unknown),
+		withNodeRenderer(ast.KindImage, orgRenderer.unknown),
+		withNodeRenderer(ast.KindLinkReferenceDefinition, orgRenderer.unknown),
+		withNodeRenderer(ast.KindStrong, orgRenderer.unknown),
+		withNodeRenderer(ast.KindThematicBreak, orgRenderer.unknown),
 	).Build()
 	return &markdownRenderer{helper}
 }
@@ -295,6 +295,10 @@ func (r *markdownRenderer) RenderStringSource(w io.Writer, source string, n ast.
 }
 
 var _ renderer.Renderer[io.Writer] = (*markdownRenderer)(nil)
+
+func withNodeRenderer(kind ast.NodeKind, fun func(io.Writer, []byte, ast.Node, bool, renderer.Context) (ast.WalkStatus, error)) renderer.Option[rendererConfig] {
+	return renderer.WithNodeRenderer[io.Writer, rendererConfig](kind, renderer.NodeRendererFunc(fun))
+}
 
 type rendererConfig struct {
 	Config renderer.Config[io.Writer, rendererConfig]
