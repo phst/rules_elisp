73691318eb6e4dbc55c3ecfc67a87a5a50c86b7e
Philipp Stephani <p.stephani2@gmail.com>
2026-09-07 17:20:23 +0200

    Move main function into separate file

--- internal/stardoc2org/generate.go
+++ internal/stardoc2org/generate.go
@@ -12,17 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Helper binary to convert Stardoc output into Org Mode format.
 package main
 
 import (
 	"errors"
-	"flag"
 	"fmt"
 	"io"
-	"log"
 	"net/url"
-	"os"
 	"regexp"
 	"runtime/debug"
 	"strings"
@@ -37,50 +33,9 @@ import (
 	"github.com/yuin/goldmark/v2/renderer"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
-	"google.golang.org/protobuf/proto"
 
 	spb "github.com/phst/rules_elisp/internal/stardoc_output_go_proto"
 )
-
-// Main function.
-func main() {
-	flag.Usage = usage
-	flag.Parse()
-	if flag.NArg() != 2 {
-		usage()
-		os.Exit(2)
-	}
-	input := flag.Arg(0)
-	output := flag.Arg(1)
-	b, err := os.ReadFile(input)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var module spb.ModuleInfo
-	if err := proto.Unmarshal(b, &module); err != nil {
-		log.Fatal(err)
-	}
-	file, err := os.OpenFile(output, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0400)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	generator := newGenerator(file)
-	if err := generator.run(&module); err != nil {
-		log.Fatal(err)
-	}
-	if err := file.Sync(); err != nil {
-		log.Fatal(err)
-	}
-	if err := file.Close(); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func usage() {
-	fmt.Fprintln(os.Stderr, "Usage: generate INPUT OUTPUT")
-	flag.PrintDefaults()
-}
 
 type generator struct {
 	file io.Writer
