7a9ca6f1da2e46593206f2c1edfbdc9f50a630cd
Philipp Stephani <p.stephani2@gmail.com>
2026-09-09 01:16:27 +0200

    Use a more common filename extension for the Go template

--- internal/stardoc/org.go
+++ internal/stardoc/org.go
@@ -39,7 +39,7 @@ func Org(module *spb.ModuleInfo, w io.Writer) error {
 	return tpl.Execute(w, module)
 }
 
-//go:embed org.template
+//go:embed org.tmpl
 var tplText string
 
 var tpl = template.Must(parseTemplate(tplText))
