4483b4681d733dd39d1f2d9de2684e200f3bac34
Philipp Stephani <p.stephani2@gmail.com>
2026-09-24 14:33:54 +0200

    Add docstring for testhelper

--- internal/testhelper/testhelper.go
+++ internal/testhelper/testhelper.go
@@ -1,4 +1,4 @@
-// Copyright 2025 Philipp Stephani
+// Copyright 2025, 2026 Philipp Stephani
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Binary testhelper is a trivial program used for testing.  It supports
+// sleeping and exiting with a specific exit code.
 package main
 
 import (
